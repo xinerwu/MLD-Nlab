b0b8ad9e2e31864fe916fd0eb388fd7439481c80
xinerwu <xiner.wu02@gmail.com>
2026-04-21 13:59:09 -0400

    Update SI model fot weighted MAT

--- MATreconstruction/run_analogue_SI.R
+++ MATreconstruction/run_analogue_SI.R
@@ -3,11 +3,15 @@
 library(analogue)
 
 dino <- read.delim('dino1968.txt',row.names = 1)
-mld <- rio::import('SI.xlsx')
-env_id <- 2:15
+mld <- rio::import('SI.xlsx',row.names = 1)
+env_id <- 2:17
 n.analogues <- 5
 
 modern <- log(dino+1)
+# Remove duplicates in the dinocyst training database
+dup <- c("1101","1102","1100","1105")
+modern <- modern[ !rownames(modern) %in% dup, ]
+mld <- mld[ !rownames(mld) %in% dup, ]
 
 ## Perform analogue matching
 #result <- analog(modern,fossil,method="euclidean")
@@ -24,8 +28,10 @@ for (i in env_id) {
   c=c+1
 }
 
-fossil <- modern
-pred_list <- lapply(mat_models,function(model) predict(model,fossil)$predictions$model$predicted[n.analogues, ])
-pred_matrix <- do.call(cbind,pred_list)
+#fossil <- modern
+#pred_list <- lapply(mat_models,function(model) predict(model,fossil)$predictions$model$predicted[n.analogues, ])
+#pred_matrix <- do.call(cbind,pred_list)
+pred_matrix <- sapply(mat_models,function(model) model$weighted$est[n.analogues, ])
 colnames(pred_matrix) <- names(mat_models)
 write.csv(pred_matrix, file = "SIrecon.csv")
+save.image("./SI_mat_models.RData")
