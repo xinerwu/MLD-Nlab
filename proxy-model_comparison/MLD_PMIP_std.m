f0daca75885733ed6c23159ea33748184fd76439
xinerwu <xiner.wu02@gmail.com>
2026-04-21 23:04:05 -0400

    Update MLD_PMIP_std.m

--- proxy-model_comparison/MLD_PMIP_std.m
+++ proxy-model_comparison/MLD_PMIP_std.m
@@ -1,4 +1,4 @@
-function MLD_PMIP_std(ax)
+function MLD_PMIP_std(ax,data)
 axes(ax);
 % Plot model standard deviation
 cmap=cmocean('matter',8);
@@ -6,7 +6,7 @@ cmap=cmocean('matter',8);
 %set(gcf, 'Position', [100, 100, 400, 440]);
 m_proj('Lambert Conformal Conic','lat',[47 85],'long',[-65 15],'rect','on')
 
-load("mean_models.mat","stdmodels","x","y")
+load(data,"stdmodels","x","y")
 h=m_pcolor(x,y,stdmodels);
 m_grid('tickdir','out','fontsize',6,'linest','none');%('linest','none','xticklabels',[],'xtick',[],'yticklabels',[],'ytick',[],'fontsize',5); %('box','on','tickdir','in','ytick',(2|[0 40]));
 m_coast('color','k');
